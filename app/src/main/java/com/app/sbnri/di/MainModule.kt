13eb491999f194fcb24d227189f51142b26f162f
siddharth <besidd2301@gmail.com>
2020-11-06 01:36:28 +0530

    provider methods added

--- app/src/main/java/com/app/sbnri/di/MainModule.kt
+++ app/src/main/java/com/app/sbnri/di/MainModule.kt
@@ -1,7 +1,52 @@
 package com.app.sbnri.di
 
+import com.app.sbnri.api.ISBNRIApi
+import com.app.sbnri.utils.Constants
 import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 class MainModule {
+
+    @Singleton
+    @Provides
+    fun providesHttpClient(): OkHttpClient {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient
+            .Builder()
+            .addInterceptor(logging)
+            .connectTimeout(1, TimeUnit.MINUTES)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun providesRetrofitInstance(client: OkHttpClient, factory: GsonConverterFactory): Retrofit {
+        return Retrofit.Builder().client(client)
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(factory)
+            .build()
+
+    }
+
+    @Singleton
+    @Provides
+    fun providesRetrofitClient(retrofit: Retrofit) : ISBNRIApi {
+        return retrofit.create(ISBNRIApi::class.java)
+    }
 }
