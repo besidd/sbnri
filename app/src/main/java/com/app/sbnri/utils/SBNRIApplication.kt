591401bfa5b493b7f593ef694b2a97c2bbc3e28e
siddharth <besidd2301@gmail.com>
2020-11-06 01:33:39 +0530

    base url added

--- app/src/main/java/com/app/sbnri/utils/SBNRIApplication.kt
+++ app/src/main/java/com/app/sbnri/utils/SBNRIApplication.kt
@@ -1,6 +1,8 @@
 package com.app.sbnri.utils
 
 import android.app.Application
+import com.app.sbnri.di.DaggerMainComponent
 
-class SBNRIApplication: Application() {
+class SBNRIApplication : Application() {
+    val appComponent = DaggerMainComponent.create()
 }
