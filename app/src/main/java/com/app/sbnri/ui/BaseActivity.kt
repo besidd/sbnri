a73ff0e5dc2ddaf11d1ca6835c79c38ef50cedcd
siddharth <besidd2301@gmail.com>
2020-11-06 00:24:12 +0530

    base activity added

--- app/src/main/java/com/app/sbnri/ui/BaseActivity.kt
+++ app/src/main/java/com/app/sbnri/ui/BaseActivity.kt
@@ -1,11 +1,10 @@
-package com.app.sbnri
+package com.app.sbnri.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+open class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/app/sbnri/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.app.sbnri.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.sbnri.R
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
