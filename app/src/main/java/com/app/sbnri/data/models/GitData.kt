b8feb856eab8d213e387a095b3c23edb702f4869
siddharth <besidd2301@gmail.com>
2020-11-09 14:19:58 +0530

    POJOs created

---
+++ app/src/main/java/com/app/sbnri/data/models/GitData.kt
@@ -0,0 +1,4 @@
+package com.app.sbnri.data.models
+
+
+class GitData(val list: List<GitDataItem>)
---
+++ app/src/main/java/com/app/sbnri/data/models/Permissions.kt
@@ -0,0 +1,7 @@
+package com.app.sbnri.data.models
+
+data class Permissions(
+    val admin: Boolean?,
+    val pull: Boolean?,
+    val push: Boolean?
+)
