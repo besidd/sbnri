6b5cef8c82d3917ade13207a162e6e8bdb564b82
siddharth <besidd2301@gmail.com>
2020-11-09 14:21:04 +0530

    db name and data count

--- app/src/main/java/com/app/sbnri/utils/Constants.kt
+++ app/src/main/java/com/app/sbnri/utils/Constants.kt
@@ -3,5 +3,7 @@ package com.app.sbnri.utils
 class Constants {
     companion object {
         const val BASE_URL = " https://api.github.com/"
+        const val DATABASE_NAME = "sbnri.db"
+        const val dataCount = 10
     }
 }
