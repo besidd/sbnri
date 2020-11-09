66c5a202889b1ad63b5de2a57815c866b1d3bd32
siddharth <besidd2301@gmail.com>
2020-11-09 14:25:14 +0530

    application class

--- app/src/main/java/com/app/sbnri/utils/SBNRIApplication.kt
+++ app/src/main/java/com/app/sbnri/utils/SBNRIApplication.kt
@@ -1,8 +1,18 @@
 package com.app.sbnri.utils
 
 import android.app.Application
+import android.content.Context
 import com.app.sbnri.di.DaggerMainComponent
 
 class SBNRIApplication : Application() {
-    val appComponent = DaggerMainComponent.create()
+    companion object {
+        lateinit var context: Context
+        lateinit var app: Application
+        val appComponent = DaggerMainComponent.create()
+    }
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+        app = this
+    }
 }
